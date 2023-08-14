d62994360177a497bd89f2624045e246b093cc79
mintsuki <mintsuki@protonmail.com>
2023-08-13 22:43:27 -0500

    Split up pkg-config CPPFLAGS and CFLAGS

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #if defined(__APPLE__)
-#  include <SDL2/SDL.h>
+#  include <SDL.h>
 #  define _XOPEN_SOURCE 600
 #else
 #  define _XOPEN_SOURCE 600
-#  include <SDL2/SDL.h>
+#  include <SDL.h>
 #endif
 
 #include <fcntl.h>
